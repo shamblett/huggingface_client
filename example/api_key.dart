a8ec11c779f27c9bfabcc1dd1b4449baa75f247a
Steve Hamblett <steve.hamblett@linux.com>
2023-08-04 10:50:58 +0100

    Add api key file

---
+++ example/api_key.dart
@@ -0,0 +1,9 @@
+/*
+ * Package : huggingface_client
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 02/08/2023
+ * Copyright :  S.Hamblett
+ */
+
+/// Your Hugging Face API key
+const apiKey = 'YOUR-API-KEY';
