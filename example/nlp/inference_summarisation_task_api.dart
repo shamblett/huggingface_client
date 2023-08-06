1a5655b5233a6da0bdaf548d7d0ff76cf18afad5
Steve Hamblett <steve.hamblett@linux.com>
2023-08-06 14:21:52 +0100

    NLP Summarisation example working, examples re structured

--- example/nlp/inference_summarisation_task_api.dart
+++ example/nlp/inference_summarisation_task_api.dart
@@ -6,41 +6,18 @@
  */
 
 import 'package:huggingface_client/huggingface_client.dart';
-import 'api_key.dart';
+import '../api_key.dart';
 
 /// Examples of how to use the Hugging Face Inference API to perform NLP
 /// task based model queries.
 
 void main() async {
-// Get an inference client with your Hugging Face API key as authentication.
+  // Get an inference client with your Hugging Face API key as authentication.
   final client = HuggingFaceClient.getInferenceClient(
       apiKey, HuggingFaceClient.inferenceBasePath);
 
-// Get an instance of the inference API using our client
+  // Get an instance of the inference API using our client
   final apiInstance = InferenceApi(client);
-
-//
-// Fill mask task
-//
-  print('');
-  print('*** Fill Mask Task ***');
-  print('');
-  try {
-    final input = 'The answer to the universe is [MASK].';
-    final params = ApiQueryNLPFillMask(inputs: [input]);
-    final result = await apiInstance.queryNLPFillMask(
-        taskParameters: params, model: 'bert-base-uncased');
-    if (result!.isNotEmpty) {
-      for (final row in result) {
-        print(row);
-      }
-    } else {
-      print('Inference task API Fill Mask returned empty result');
-    }
-  } catch (e) {
-    print('Exception when calling Inference task API Fill Mask: $e - exiting');
-    return;
-  }
 
   //
   // Text summarisation task
@@ -62,7 +39,7 @@ void main() async {
         taskParameters: params, model: 'facebook/bart-large-cnn');
     if (result!.isNotEmpty) {
       for (final row in result) {
-        print(row);
+        print(row?.summaryText);
       }
     } else {
       print('Inference task API Text Summarisation returned empty result');
