b54a4e0784ccb49e9737b3a00f83f0be9240d61a
Steve Hamblett <steve.hamblett@linux.com>
2023-09-03 10:54:11 +0100

    Inference endpoint examples complete

--- example/inference_endpoint_api.dart
+++ example/inference_endpoint_api.dart
@@ -20,20 +20,6 @@ void main() async {
   final apiInstance = EndpointApi(client);
 
   //
-  // List endpoints
-  //
-  print('');
-  print('*** Endpoint API list endpoints ***');
-  print('');
-  try {
-    final result = await apiInstance.listEndpoint();
-    print(result);
-  } catch (e) {
-    print('Exception when calling Endpoint API List Endpoints: $e - exiting');
-    return;
-  }
-
-  //
   // Create an endpoint
   //
   print('');
@@ -50,17 +36,151 @@ void main() async {
     final model = EndpointModel(
         framework: EndpointFramework.custom,
         image: endpointModelImage,
-        repository: 'gpt2');
+        repository: 'gpt2',
+        revision: 'REV 1');
     final endpoint = Endpoint(
         compute: compute,
         model: model,
         name: 'SJH Test',
         provider: EndpointProvider(region: 'us-east-1', vendor: 'aws'),
-        type: EndpointType.private);
+        type: EndpointType.protected);
     final result = await apiInstance.createEndpoint(endpoint);
     print(result);
   } catch (e) {
     print('Exception when calling Endpoint API Create Endpoint: $e - exiting');
+    return;
+  }
+
+  //
+  // List endpoints
+  //
+  print('');
+  print('*** Endpoint API list endpoints ***');
+  print('');
+  try {
+    final result = await apiInstance.listEndpoint();
+    print(result);
+  } catch (e) {
+    print('Exception when calling Endpoint API List Endpoints: $e - exiting');
+    return;
+  }
+
+  //
+  // Get endpoint information
+  //
+  print('');
+  print('*** Endpoint API get endpoint information ***');
+  print('');
+  try {
+    final result = await apiInstance.getEndpoint('SJH Test');
+    print(result);
+  } catch (e) {
+    print(
+        'Exception when calling Endpoint API GetEndpoint Information: $e - exiting');
+    return;
+  }
+
+  //
+  // Update  an endpoint
+  //
+  print('');
+  print('*** Endpoint API update endpoint ***');
+  print('');
+  try {
+    final compute = EndpointComputeUpdate(instanceType: 'c5i');
+    final model = EndpointModelUpdate(revision: 'REV 2');
+    final endpoint = EndpointUpdate(compute: compute, model: model);
+    final result = await apiInstance.updateEndpoint('SJH Test', endpoint);
+    print(result);
+  } catch (e) {
+    print('Exception when calling Endpoint API Update Endpoint: $e - exiting');
+    return;
+  }
+
+  //
+  // Get endpoint logs
+  //
+  print('');
+  print('*** Endpoint API get endpoint logs ***');
+  print('');
+  try {
+    final result = await apiInstance.getEndpointLogs('SJH Test', tail: 20);
+    print(result);
+  } catch (e) {
+    print(
+        'Exception when calling Endpoint API Get Endpoint Logs: $e - exiting');
+    return;
+  }
+
+  //
+  // Get endpoint metric
+  //
+  print('');
+  print('*** Endpoint API get endpoint metric ***');
+  print('');
+  try {
+    await apiInstance.getEndpointMetric('SJH Test', 'request-count');
+    print('Endpoint aggregated metrics returned successfully');
+  } catch (e) {
+    print(
+        'Exception when calling Endpoint API Get Endpoint Metric: $e - exiting');
+    return;
+  }
+
+  //
+  // Pause endpoint
+  //
+  print('');
+  print('*** Endpoint API pause endpoint ***');
+  print('');
+  try {
+    final result = await apiInstance.pauseEndpoint('SJH Test');
+    print(result);
+  } catch (e) {
+    print('Exception when calling Endpoint API Pause Endpoint: $e - exiting');
+    return;
+  }
+
+  //
+  // Resume endpoint
+  //
+  print('');
+  print('*** Endpoint API resume endpoint ***');
+  print('');
+  try {
+    final result = await apiInstance.resumeEndpoint('SJH Test');
+    print(result);
+  } catch (e) {
+    print('Exception when calling Endpoint API Resume Endpoint: $e - exiting');
+    return;
+  }
+
+  //
+  // Scale endpoint to zero
+  // This API call exists in the swagger docs but not in the openapi spec.
+  //
+  print('');
+  print('*** Endpoint API scale endpoint to zero ***');
+  print('');
+  try {
+    print('API call does not exist in the Open API specification');
+  } catch (e) {
+    print(
+        'Exception when calling Endpoint API Scale Endpoint To Zero: $e - exiting');
+    return;
+  }
+
+  //
+  // Delete endpoint
+  //
+  print('');
+  print('*** Endpoint API delete endpoint ***');
+  print('');
+  try {
+    await apiInstance.deleteEndpoint('SJH Test');
+    print('Endpoint deleted successfully');
+  } catch (e) {
+    print('Exception when calling Endpoint API Delete Endpoint : $e - exiting');
     return;
   }
 
